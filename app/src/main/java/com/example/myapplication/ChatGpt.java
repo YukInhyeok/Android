78ee18512ec82a5a966cceff58abb968634b9407
Inhyeok <ddolmin5155@gmail.com>
2023-07-14 18:25:01 +0900

    수정

--- app/src/main/java/com/example/myapplication/ChatGpt.java
+++ app/src/main/java/com/example/myapplication/ChatGpt.java
@@ -47,12 +47,13 @@ public class ChatGpt extends AppCompatActivity {
 
     List<Message> messageList;
     MessageAdapter messageAdapter;
+    JSONArray messages = new JSONArray();
 
 
     // API 호출에 사용할 상수와 객체를 선언합니다.
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     OkHttpClient client;
-    private static final String MY_SECRET_KEY = "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";
+    private static final String MY_SECRET_KEY = "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";
 
     //네비게이션바 설정
     private BottomNavigationView bottomNavigationView;
@@ -125,7 +126,7 @@ public class ChatGpt extends AppCompatActivity {
                     // "GPT" 아이템 클릭 시 동작
                     Intent intent1 = new Intent(ChatGpt.this, MainActivity.class);
                     startActivity(intent1);
-                } else if (itemId== R.id.menu_book) {
+                } else if (itemId == R.id.menu_book) {
                     // "Book" 아이템 클릭 시 동작
                     Intent intent2 = new Intent(ChatGpt.this, BookMainActivity.class);
                     startActivity(intent2);
@@ -142,7 +143,7 @@ public class ChatGpt extends AppCompatActivity {
     }
 
     // 대화 내용을 채팅창에 추가하는 메소드입니다.
-    void addToChat(String message, String sentBy){
+    void addToChat(String message, String sentBy) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -154,80 +155,87 @@ public class ChatGpt extends AppCompatActivity {
     }
 
     // GPT의 응답을 채팅창에 추가하는 메소드입니다.
-    void addResponse(String response){
-        messageList.remove(messageList.size()-1);
+    void addResponse(String response) {
+        messageList.remove(messageList.size() - 1);
         addToChat(response, Message.SENT_BY_BOT);
     }
 
     // GPT API를 호출하여 사용자의 메시지를 전달하고 응답을 받는 메소드입니다.
-    void callAPI(String question){
+    void callAPI(String question) {
         //okhttp
         messageList.add(new Message("...", Message.SENT_BY_BOT));
 
-        // API 요청에 필요한 데이터를 생성합니다.
+        //추가된 내용
         JSONArray arr = new JSONArray();
         JSONObject baseAi = new JSONObject();
         JSONObject userMsg = new JSONObject();
         try {
-            // AI 속성 설정
-            baseAi.put("role", "system");
-            baseAi.put("content", "지금부터 당신은 한국어 선생님입니다. 당신은 사용자와의 10마디 대화를 통해 사용자의 한국어 문법 능력을 테스트를 해야 합니다. 점수 측정은 100점에서 한 문제를 틀릴 때마다 10점씩 차감되는 형태로 진행하며 10마디 대화 이후엔 어떠한 대화를 하고 있던 사용자의 한국어 능력 점수를 알려주어야 합니다.");
-
-            // 유저 메시지
+            //AI 속성설정
+            baseAi.put("role", "user");
+            baseAi.put("content", "당신은 사용자와의 10가지의 국어 문제를 통해 사용자의 국어 능력을 테스트를 해야 합니다." +
+                    "점수 측정은 100점이 기준입니다. 한 문제를 틀릴 때마다 10점씩 차감합니다." +
+                    "10가지의 문제는 무조껀 한개씩 진행되어야 합니다." + "사용자의 대답을 듣고 정답 인지 오답 인지 판별하여 알려주어야합니다." +
+                    "사용자가 대화를 보내기 전까지 정답을 절대 알려주면 안됩니다." +
+                    "그 후 두번 째 문제를 내고 사용자의 대답을 듣고, 정답 인지 오답 인지 판별 하고 점수를 계산하는 행동을 열 번째 문제까지 반복합니다." +
+                    "10가지의 문제가 끝나면 사용자에게 점수를 꼭 알려주어야 합니다.");
+            //유저 메세지
             userMsg.put("role", "user");
-            userMsg.put("content", "당신은 한국어 선생님입니다. 당신은 지금부터 저와 대화를 통해서 저의 한국어 능력 점수를 평가해야합니다. 점수는 100점부터 틀릴 때마다 10점씩 차감되며 당신은 틀린 이유와 함께 현재 점수를 알려주어야합니다.");
-
-            // array로 담아서 한번에 보냅니다.
-            arr.put(baseAi);
-            arr.put(userMsg);
-        } catch (JSONException e){
+            userMsg.put("content", question);
+            //array로 담아서 한번에 보낸다
+            if (messages.length() == 0)
+                messages.put(baseAi);
+            messages.put(userMsg);
+        } catch (JSONException e) {
             throw new RuntimeException(e);
         }
 
-        // API 요청에 필요한 JSON 데이터를 생성합니다.
         JSONObject object = new JSONObject();
         try {
+            //모델명 변경
             object.put("model", "gpt-3.5-turbo");
-            object.put("messages", arr);
-        } catch (JSONException e){
+            object.put("messages", messages);
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         RequestBody body = RequestBody.create(object.toString(), JSON);
-
-        // API 요청을 생성합니다.
         Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/chat/completions")
-                .header("Authorization", "Bearer "+MY_SECRET_KEY)
+                .url("https://api.openai.com/v1/chat/completions")  //url 경로 수정됨
+                .header("Authorization", "Bearer " + MY_SECRET_KEY)
                 .post(body)
                 .build();
 
-        // API 요청을 비동기적으로 실행합니다.
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                addResponse("실패1 "+e.getMessage());
-
+                addResponse("Failed to load response due to " + e.getMessage());
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    // API 응답을 파싱하여 GPT의 응답을 가져옵니다.
+                if (response.isSuccessful()) {
                     JSONObject jsonObject = null;
                     try {
                         jsonObject = new JSONObject(response.body().string());
                         JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        // 아래 result 받아오는 경로가 좀 수정되었다.
                         String result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content");
-                        addResponse(result.trim());
-//                        DBHelper dbHelper = new DBHelper(ChatGpt.this);
-//                        dbHelper.addMessage(question, Message.SENT_BY_ME);
-//                        dbHelper.addMessage(response, Message.SENT_BY_BOT);
 
-                    }catch (JSONException e){
+                        // 채팅창에 추가한 뒤에, 메시지 기록에 AI의 응답을 저장합니다.
+                        addResponse(result.trim());
+                        JSONObject assistantMsg = new JSONObject();
+                        try {
+                            assistantMsg.put("role", "assistant");
+                            assistantMsg.put("content", result.trim());
+                            messages.put(assistantMsg);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 } else {
-                    addResponse("실패2"+response.body().string());
+                    addResponse("Failed to load response due to " + response.body().string());
                 }
             }
         });
