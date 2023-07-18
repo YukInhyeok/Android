b37fb1c4cb6684c128dde8b8f984f0cffc8c2887
Inhyeok <ddolmin5155@gmail.com>
2023-07-18 12:30:32 +0900

    Firebase에 자유, 통제 (0, 1) 으로 저장.

--- app/src/main/java/com/example/myapplication/screen/ScreenOnReceiver.java
+++ app/src/main/java/com/example/myapplication/screen/ScreenOnReceiver.java
@@ -1,10 +1,18 @@
 package com.example.myapplication.screen;
 
+import static androidx.core.app.ActivityCompat.finishAffinity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.myapplication.MainActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ScreenOnReceiver extends BroadcastReceiver {
     @Override
@@ -22,10 +30,15 @@ public class ScreenOnReceiver extends BroadcastReceiver {
             Log.d("ScreenOnReceiver", "timeValue: " + timeValue);
             Log.d("ScreenOnReceiver", "workNum: " + workNum);
 
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
             if (Long.parseLong(formattedAppUsageTime) < timeValue || finishBooknum < workNum) {
                 Intent lockScreenIntent = new Intent(context.getApplicationContext(), lockscreen.class);
                 lockScreenIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(lockScreenIntent);
+
+                db.collection("Screen").document("Lock").update("state", 0);
+            }else{
+                db.collection("Screen").document("Lock").update("state", 1);
             }
         }
     }
